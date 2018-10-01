c00c90497e39248a996733765d402afd46133f53
Tien Tran <tientran2407.vn@gmail.com>
2018-10-01 23:25:16 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,9 +8,10 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  list = ""
   passengers.each do |suite, name|
     if suite == suite_a && name.start_with?("A")
-      puts name
+      
     end
   end
 end
